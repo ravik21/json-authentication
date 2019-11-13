4db86ccbed91835b232f301963db014b4e14d659
Ravi <webartisan.ravi@gmail.com>
2019-11-13 19:26:05 +0530

    Login controller api generated

--- app/Http/Controllers/Api/Auth/LoginController.php
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -7,8 +7,40 @@ use Illuminate\Http\Request;
 use App\Http\Requests\LoginRequest;
 use Auth;
 
+  /**
+   * @SWG\Swagger(
+   *   basePath="/api",
+   *   @SWG\Info(
+   *     title="Login request",
+   *     version="1.0.0"
+   *   )
+   * )
+   */
+
 class LoginController extends Controller
 {
+  /**
+   * @SWG\Post(
+   *     path="/login",
+   *     summary="Login with credentials.",
+   *     tags={"Auth"},
+   *     description="Permits an Authorization attempt.",
+   *     operationId="authLogin",
+   *      @SWG\Parameter(
+   *       name="payload",
+   *       in="formData",
+   *       description="Payload must contian employee id and password.",
+   *       required=true,
+   *       type="string",
+   *         @SWG\Schema(
+   *              example={ "employee_id": 1234567, "password":123456}
+   *         ),
+   *   ),
+   *     @SWG\Response(response="200",description="Successful operation."),
+   *     @SWG\Response(response="422",description="Validation error."),
+   *     @SWG\Response(response="401",description="Unauthorized User.")
+   * )
+   */
     public function login(LoginRequest $request)
     {
         $credentials = $request->only('employee_id', 'password');
@@ -20,6 +52,26 @@ class LoginController extends Controller
         return $this->respondWithToken($token);
     }
 
+    /**
+    * @SWG\Get(
+    *     path="/logout",
+    *     summary="Destroys a Access Token.",
+    *     tags={"Auth"},
+    *     description="Destroys the given Access Token.",
+    *     operationId="authLogout",
+    *     produces={"application/json"},
+    *     @SWG\Parameter(
+    *         name="Authorization",
+    *         in="header",
+    *         description="Valid User Token for logged in User.",
+    *         required=true,
+    *         type="string",
+    *         @SWG\Items(type="string"),
+    *     ),
+    *     @SWG\Response(response="200",description="Successfully logged out."),
+    *     @SWG\Response(response="401",description="Unauthorized User."),
+    * )
+    */
     public function logout()
     {
         Auth::logout();
